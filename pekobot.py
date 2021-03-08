2d0609d0f834785e0a1bfd8af30f345833e8ded3
BowenSim <e0415436@u.nus.edu>
2021-03-08 20:22:59 +0800

    remove guild roster

--- pekobot.py
+++ pekobot.py
@@ -32,8 +32,8 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})\n'
     )
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+"""     members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}') """
 
 
 """ Triggers upon memeber join event """
