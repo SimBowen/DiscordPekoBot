f6d63bb9c94524887c907bccda5f759ff3abc5c8
BowenSim <e0415436@u.nus.edu>
2021-03-08 17:42:12 +0800

    Pekobot now responds to haha and tries to play a pekora laugh video

--- pekobot.py
+++ pekobot.py
@@ -5,21 +5,22 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-
 load_dotenv()
+
+""" Gets the discord bot token and server name from .env file """
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+""" Intenets are required for bot to see memebrs in the server as of latest discord.py """
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!peko')
-
-previous_message = 'test'
 
 
+""" Upon client startup event run the following code """
 @client.event
 async def on_ready():
+    """ Loop thorugh to find server names if required """
     for guild in client.guilds:
         if guild.name == GUILD:
             break
@@ -42,6 +43,7 @@ async def on_ready():
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
+""" Triggers upon memeber join event """
 @client.event
 async def on_member_join(member):
     await member.create_dm()
@@ -49,6 +51,7 @@ async def on_member_join(member):
         f'こんにちは {member.name}, いらっしゃいぺこ!'
     )
 
+""" Triggers upon message and checks current message """
 
 """ @client.event
 async def on_message(message):
@@ -60,12 +63,18 @@ async def on_message(message):
         print(response)
         await message.channel.send(response) """
 
+""" Triggers on message, checks if there were previous messages to parse."""
 @client.event
 async def on_message(message):
+    if 'haha' in message.content:
+        response = "AH↗️HA↘️HA↗️HA↘️HA↗️HA↘️HA↗️HA↘️"
+        await message.channel.send(response)
+        channel= client.get_channel(818414256612573194)
+        await channel.send("!p pekora laugh")
+
     c_channel = discord.utils.get(message.guild.text_channels, name='general')
+    """ Returns a list of limit 2 messages """
     messages = await c_channel.history(limit=2).flatten()
-    if(messages[1] != NULL):
-        print(messages[1].content)
     if messages[0].content == '!peko':
         if(messages[1] == NULL):
             print('nothing')
@@ -77,19 +86,18 @@ async def on_message(message):
 
 
 
-    
-
-
+""" pekofy splitter """
 def pekofy(input):
-    output = ""
-    x = input.split(".")
-    if(len(x)==0):
-        output = input + " peko."
+    output = input
+    delimiters = ['.',',','!','?']
+    res = any(ele in delimiters for ele in input)
+    if res:
+        output = output.replace("."," peko.")
+        output = output.replace(","," peko,")
+        output = output.replace("!"," peko!")
+        output = output.replace("?"," peko?")
     else:
-        for word in x:
-            if word == '':
-                continue
-            output += word + " peko."
+        output += " peko."
     return output
 
 
