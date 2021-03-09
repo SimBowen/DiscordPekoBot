de4170a01ce7c21c9a1649c19da02332bedba180
BowenSim <e0415436@u.nus.edu>
2021-03-09 13:23:31 +0800

    added yt playing functionality

--- pekobot.py
+++ pekobot.py
@@ -2,13 +2,18 @@
 
 """ git commit the changes first. 'git push heroku master' to update server build. 'heroku logs -a pekobotnewob' to see logs """
 
+import asyncio
+from asyncio.tasks import current_task
+
+from discord.errors import ClientException
 import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.ext.tasks import loop
 from asyncio import sleep
 import random
-
+import youtube_dl
 load_dotenv()
 
 """ Gets the discord bot token and server name from .env file """
@@ -20,7 +25,7 @@ intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix='!')
+yt_list = []
 
 
 """ Upon client startup event run the following code """
@@ -128,6 +133,43 @@ async def on_message(message):
     if message.content == '!peko':
         print(pekofy(messages[1].content))
         await message.channel.send(pekofy(messages[1].content))
+    
+    if message.content[0:6] == '!pekop':
+        url = message.content[7:]
+        print(url)
+        player = await YTDLSource.from_url(url, loop=client.loop)
+        try:
+            voice_channel = message.author.voice.channel
+        except AttributeError:
+            voice_channel = None
+        yt_list.append([voice_channel,message.channel, player])
+
+
+
+
+
+@loop(seconds=1)
+async def yt_player():
+    await client.wait_until_ready()
+    if yt_list:
+        current_track = yt_list[0]
+        voice_channel = current_track[0]
+        channel = None
+        if voice_channel != None:
+            channel = voice_channel.name
+            try:
+                vc = await voice_channel.connect()
+            except ClientException:
+                print(yt_list)
+                return
+            vc.play(current_track[2], after=lambda e: print('Player error: %s' % e) if e else None)
+            await current_track[1].send('Now playing: {}'.format(current_track[2].title))
+            yt_list.pop(0)
+            while vc.is_playing():
+                    await sleep(1)
+            await vc.disconnect()
+
+yt_player.start()
 
 
 
@@ -145,6 +187,51 @@ def pekofy(input):
     else:
         output += " peko."
     return output
+
+
+
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+        
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 client.run(TOKEN)
