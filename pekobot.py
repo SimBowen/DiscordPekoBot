5870890471355de2f95767f42edc2b6a4e1195b8
BowenSim <e0415436@u.nus.edu>
2021-06-13 13:18:30 +0800

    raid

--- pekobot.py
+++ pekobot.py
@@ -138,6 +138,45 @@ async def on_message(message):
         entry = int(message.content[6:])
         playlist.pop(entry)
         await print_playlist(playlist, message.channel)
+    
+    if "!raid" in message.content.lower() and message.author != client.user:
+        by = message.author
+        input = message.content.lower()
+        elements = input.split(' ')
+        try:
+            level = elements[1]
+            float(level)
+        except:
+            await message.reply("Invalid raid command format!")
+            return
+        GMT8 = pytz.timezone('Asia/Singapore')
+        time = datetime.now(GMT8)
+        embed=discord.Embed(title="Raid Alert!", url="", description="This is a raid alert. Kindly enter and wait for 3 minutes before killing the raid boss!", color=0xFF5733)
+        embed.set_author(name=by.name, url="", icon_url=by.avatar_url)
+        embed.set_thumbnail(url="https://i.imgur.com/xpUtROZ.png")
+        embed.add_field(name="Level", value=level, inline=True)
+        embed.add_field(name="Time Sent (GMT+8)", value=time.strftime('%Y-%m-%d %H:%M:%S'), inline=True)
+        embed.add_field(name="Status", value="Valid")
+        embed.set_footer(text="This message will time out 3 minutes after the notices has been sent!")
+        alert = await message.channel.send(embed=embed)
+        await alert.add_reaction('👍')
+        embedupdate=discord.Embed(title="Old Raid", url="", description="This is an old raid alert.", color=0xFF5733)
+        embedupdate.set_author(name=by.name, url="", icon_url=by.avatar_url)
+        embedupdate.set_thumbnail(url="https://i.imgur.com/xpUtROZ.png")
+        embedupdate.add_field(name="Level", value=level, inline=True)
+        embedupdate.add_field(name="Time Sent (GMT+8)", value=time.strftime('%Y-%m-%d %H:%M:%S'), inline=True)
+        embedupdate.add_field(name="Status", value="Expired")
+        def check(reaction, user):
+            return user == message.author and str(reaction.emoji) == '👍' and reaction.message == alert
+        try:
+            await client.wait_for('reaction_add', timeout=180.0, check=check)
+            embedupdate.set_footer(text="Raid boss has been killed!")
+            await alert.edit(embed = embedupdate)
+            return 
+        except asyncio.TimeoutError:
+            embedupdate.set_footer(text="3 minutes has elapsed. Raid boss may have been killed!")
+            await alert.edit(embed = embedupdate)
+            return 
         
 
 
